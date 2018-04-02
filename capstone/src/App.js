ef60073d165fe37b3517ac81bd8c537201526330
nothingtop <henry.chen414@gmail.com>
2018-04-02 13:03:07 -0400

    wip

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <div className="row" style={{display: 'flex', 'flex-direction':'row', padding:'20px', height:'40%'}}>
+          <div className="row" style={{display: 'flex', 'flex-direction':'row', padding:'20px', height:'40%', 'padding-bottom': '100px'}}>
               <div style={{width: '80%', height:'100%'}}>
                 <SsimRangeChart />
               </div>
@@ -29,20 +29,58 @@ class App extends Component {
 }
 
 class Images extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { currentCount: 0, panels: []};
+        this.fetchData = this.fetchData.bind(this);
+        this.fetchPanels = this.fetchPanels.bind(this);
+    }
+
+    componentDidMount() {
+        this.refreshTimer = setInterval(this.fetchData, 4000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.refreshTimer);
+    }
+
+    fetchData() {
+        //simulate API fetch from server - we'll just create a list with random prices
+        var that = this;
+        fetch(url + 'getUpdateCount').then(response =>
+            response.json().then(data => ({
+                    data: data,
+                    status: response.status
+                })
+            ).then(res => {
+                if (Number(res.data.count) > that.state.currentCount){
+                    alert("got higher count");
+                    that.setState({currentCount: Number(res.data.count)});
+                    that.fetchPanels();
+                }
+            })
+        );
+    }
+
+    fetchPanels(){
+        var array = ["original", "input", "output"];
+        for(let i=0; i<Math.min(5, this.state.currentCount - this.state.panels.length) ;i++){
+            var images = array.map(image => {
+                let imageStr = url + 'getFile?fileDir=' + image + '&&index=' + i;
+                return  <div className="col-sm-4"><Panel><Panel.Heading><Panel.Title componentClass="h3">{image}</Panel.Title></Panel.Heading><Panel.Body><img key={image} src={imageStr} alt="" className="img-responsive" style={{width: '100%', height: '100%'}} /></Panel.Body></Panel></div>
+            });
+            var newArray = this.state.panels.slice();
+            newArray.unshift(images);
+            this.setState({panels: newArray});
+            alert(this.state.panels.length);
+        }
+    }
+
   render() {
 
-      var array = ["original", "input", "output"];
-      var panels = [];
-      for(let i=0; i<5 ;i++){
-        var images = array.map(image => {
-        let imageStr = url + 'getFile?fileDir=' + image + '&&index=' + i;
-        return  <div className="col-sm-4"><Panel><Panel.Heading><Panel.Title componentClass="h3">{image}</Panel.Title></Panel.Heading><Panel.Body><img key={image} src={imageStr} alt="" className="img-responsive" style={{width: '100%', height: '100%'}} /></Panel.Body></Panel></div>
-        });
-        panels.push(images);
-      }
       return (
         <div className="row">
-            {panels}
+            {this.state.panels}
           </div>
       );
   }
